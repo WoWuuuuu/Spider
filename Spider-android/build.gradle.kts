76b4e2d2f82a9ab165b2e173600832c8cf2afcd3
WoWuuuuu <WoWuuuuu@users.noreply.github.com>
2026-08-27 20:31:54 +0800

    Migrate Android codebase to NekoBox v1.4.2 with Spider branding

--- Spider-android/build.gradle.kts
+++ Spider-android/build.gradle.kts
@@ -1,24 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+allprojects {
+    apply(from = "${rootProject.projectDir}/repositories.gradle.kts")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
+
 plugins {
-    id("com.android.application") version "9.0.1" apply false
-    id("com.android.library") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.10" apply false
-    id("com.google.devtools.ksp") version "2.3.5" apply false
-    id("com.github.triplet.play") version "4.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
-    alias(libs.plugins.spotless) apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.8"
-}
-
-detekt {
-    buildUponDefaultConfig = true
-    allRules = false
-    config.setFrom("${projectDir}/config/detekt/detekt.yml")
-    baseline = file("${projectDir}/config/detekt/baseline.xml")
-    source.setFrom("app/src/main/java", "app/src/main/kotlin")
-}
-
-dependencies {
-    "detektPlugins"("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
